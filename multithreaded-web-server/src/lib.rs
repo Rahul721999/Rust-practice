11352aee9bf21f2b82ee4c06b02ca98bfdb1c079
Rahul721999 <rrmondal12@gmail.com>
2023-03-15 18:20:14 +0530

    working on multithreaded web server

--- multithreaded-web-server/src/lib.rs
+++ multithreaded-web-server/src/lib.rs
@@ -1,43 +1,69 @@
-use std::{thread::{JoinHandle, self}};
-use anyhow::{Result, anyhow};
+use anyhow::{anyhow, Result};
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+use std::thread::{self, JoinHandle};
 
-const MAX_THREAD_LIMIT : usize = 4;
-pub struct ThreadPool{
-    thread : Vec<thread::JoinHandle<()>>
+const MAX_THREAD_LIMIT: usize = 5;
+struct Worker {
+    id: usize,
+    thread: JoinHandle<()>,
 }
 
-impl ThreadPool{
-    pub fn new(size: usize) -> Result<ThreadPool, anyhow::Error>{
-        match size{
+
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = receiver
+                .lock()
+                .expect("failed to lock the reciver") // have to use Mutex locking mechanism
+                .recv()
+                .expect("failed on recv method");
+
+            println!("Worker {id} got a job; executing...");
+            job();
+        });
+        Worker { id: id, thread }
+    }
+}
+
+// struct Job;
+type Job = Box<dyn FnOnce() + Send + 'static>;
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+impl ThreadPool {
+    pub fn new(size: usize) -> Result<ThreadPool, anyhow::Error> {
+        match size {
             0 => return Err(anyhow!("zero thread cannot be created")),
             n => {
-                    if n >= MAX_THREAD_LIMIT{
-                    return Err(anyhow!("Thread is not available"));
-                }
-                else{
+                if n >= MAX_THREAD_LIMIT {
+                    return Err(anyhow!("Max Thread Spawn Limit is < 5"));
+                } else {
+                    // creating a mpsc channel to pass the data between thread
+                    let (sender, reciever) = mpsc::channel();
+                    // wrapping reciever therad with Arc pointer
+                    // cause the Job doesn't impl copy trait
+                    let reciever = Arc::new(Mutex::new(reciever));
                     let mut threads = Vec::with_capacity(size);
-                    for n in 0..size{
-                        let t = ThreadPool::spawn(size);
-                        threads.push(t);
+                    for i in 0..size {
+                        // creating thread and pushing to the vector
+                        threads.push(Worker::new(i, reciever.clone()));
                     }
-                    return Ok(ThreadPool { thread: threads})
+                    Ok(ThreadPool {
+                        workers: threads,
+                        sender,
+                    })
                 }
             }
         }
     }
     pub fn execute<F>(&self, f: F)
     where
-        F : FnOnce() + 'static + Send,
-        {
-
-        }
-    
-    pub fn spawn<F, T>(f : F) -> JoinHandle<T>
-    where  
-        F : FnOnce() -> T,
-        F : Send + 'static,
-        T : Send + 'static,
+        F: FnOnce() + 'static + Send,
     {
-
-    } 
+        let job = Box::new(f);
+        self.sender.send(job).expect("failed to send the job to the channel");
+    }
 }
--- multithreaded-web-server/src/main.rs
+++ multithreaded-web-server/src/main.rs
@@ -3,32 +3,46 @@ use std::{
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
+
+use multithreaded_web_server::ThreadPool;
 fn main() {
     let listen = TcpListener::bind("127.0.0.1:8080").expect("failed to get the address");
+    let pool =match ThreadPool::new(4){
+        Ok(t) => t,
+        Err(err) => {
+            println!("{err}");
+            return
+        }
+    };
+
     for stream in listen.incoming() {
-        let _stream = stream.expect("failed to get the stream");
-        std::thread::spawn(||{
-            handle_req(_stream);
+        let stream = stream.expect("failed to get the stream");
+        pool.execute(||{
+            handle_req(stream);
         });
     }
 }
 
 fn handle_req(mut stream: TcpStream) {
     let buf = BufReader::new(&mut stream);
-    let http_req = buf.lines().next().expect("failed on first unwrap").expect("failed on sec unwrap");
+    let http_req = buf
+        .lines()
+        .next()
+        .expect("failed on first unwrap")
+        .expect("failed on sec unwrap");
 
-    let (status, filename) = match &http_req [..]{
+    let (status, filename) = match &http_req[..] {
         "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
         "GET /sleep HTTP/1.1" => {
             std::thread::sleep(std::time::Duration::from_secs(15));
             ("HTTP/1.1 200 OK", "hello.html")
-        },
-        _=> ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    };  
-    
-        let content = fs::read_to_string(filename).expect("failed to get html file");
-        let length = content.len();
-        let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
 
-        stream.write_all(response.as_bytes()).unwrap();
+    let content = fs::read_to_string(filename).expect("failed to get html file");
+    let length = content.len();
+    let response = format!("{status}\r\nContent-Length: {length}\r\n\r\n{content}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }

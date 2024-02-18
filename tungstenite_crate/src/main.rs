5c7d65e36a0b42742eff8453486fad71a7785017
Rahul721999 <rrmondal12@gmail.com>
2024-02-18 18:10:36 +0530

    WIP: implementing ws for web3 service

--- tungstenite_crate/src/main.rs
+++ tungstenite_crate/src/main.rs
@@ -1,3 +1,4 @@
+mod model;
 use std::{
     collections::HashMap,
     env,
@@ -21,14 +22,15 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
     let ws_stream = tokio_tungstenite::accept_async(raw_stream)
         .await
         .expect("Error during the websocket handshake occurred");
-    println!("WebSocket connection established: {}", addr);
+    println!("WebSocket connection established with address: {}", addr);
 
-    // Insert the write part of this peer to the peer map.
-    let (tx, rx) = unbounded();
-    peer_map.lock().unwrap().insert(addr, tx);
+    // Create a channel for our stream, which other sockets will use to send us messages.
+    let (sender, reciever) = unbounded();
+    peer_map.lock().unwrap().insert(addr, sender);
 
     let (outgoing, incoming) = ws_stream.split();
 
+    // handle revcieved messages event
     let broadcast_incoming = incoming.try_for_each(|msg| {
         println!("Received a message from {}: {}", addr, msg.to_text().unwrap());
         let peers = peer_map.lock().unwrap();
@@ -37,14 +39,20 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
         let broadcast_recipients =
             peers.iter().filter(|(peer_addr, _)| peer_addr != &&addr).map(|(_, ws_sink)| ws_sink);
 
+        // TODO: broadcasting the message to all the peers now  
+        // ! But we need to send the message to the matchEngine
         for recp in broadcast_recipients {
+            println!("Sending message to {:?}", recp.clone());
             recp.unbounded_send(msg.clone()).unwrap();
         }
 
         future::ok(())
+
+
+        // ! Remember we don't need the functionality to send message
     });
 
-    let receive_from_others = rx.map(Ok).forward(outgoing);
+    let receive_from_others = reciever.map(Ok).forward(outgoing);
 
     pin_mut!(broadcast_incoming, receive_from_others);
     future::select(broadcast_incoming, receive_from_others).await;
@@ -54,7 +62,7 @@ async fn handle_connection(peer_map: PeerMap, raw_stream: TcpStream, addr: Socke
 }
 
 #[tokio::main]
-async fn main() ->  Result<(), IoError> {
+async fn main() -> Result<(), IoError> {
     let addr = env::args().nth(1).unwrap_or_else(|| "127.0.0.1:8080".to_string());
 
     let state = PeerMap::new(Mutex::new(HashMap::new()));
@@ -70,4 +78,45 @@ async fn main() ->  Result<(), IoError> {
     }
 
     Ok(())
+}
+
+// taker / maker / mactchEngine should have a id.
+// will have to create an enum to handle the different types of messages
+// message from taker will go to the matchEngine
+// message from maker will go to the matchEngine
+// message from matchEngine will go to the taker or maker
+
+#[derive(Debug, Clone, Copy)]
+pub enum PayLoad{
+    Subscribe,
+    TakerMsg,
+    MakerMsg,
+    MatchEngineMsg
+}
+
+/*
+    Subscribe payload{
+        msg_type : "subscribe",
+        name_of_sender : "Taker" / "maker" / "matchEngine",
+        id : "id",
+    }
+*/
+
+
+fn handle_message(payload: PayLoad, msg: String){
+    match payload{
+        PayLoad::Subscribe => {
+            // TODO : Store the ip address   
+            println!("Subscribe payload");
+        },
+        PayLoad::TakerMsg => {
+            println!("TakerMsg payload");
+        },
+        PayLoad::MakerMsg => {
+            println!("MakerMsg payload");
+        },
+        PayLoad::MatchEngineMsg => {
+            println!("MatchEngineMsg payload");
+        }
+    }
 }
---
+++ tungstenite_crate/src/model.rs
@@ -0,0 +1,17 @@
+use std::{
+    collections::HashMap,
+    env,
+    io::Error as IoError,
+    net::SocketAddr,
+    sync::{Arc, Mutex},
+};
+
+use futures_channel::mpsc::{unbounded, UnboundedSender};
+use futures_util::{future, pin_mut, stream::TryStreamExt, StreamExt};
+
+use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::tungstenite::protocol::Message;
+
+type Tx = UnboundedSender<Message>;
+type PeerMap = Arc<Mutex<HashMap<SocketAddr, Tx>>>;
+
